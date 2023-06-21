a1f5e1f46e1538d0676bc110708aec657385fcfb
dardan743 <dardan.llapashtica@riinvest.net>
2023-06-21 03:43:14 +0200

    updated routes

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,5 +1,7 @@
 export const HOME = "/";
-export const CSGO = "/csgo-settings-swag";
+export const CSGO = "/csgo";
+export const PUBG = "/pubg";
+export const FORTNITE = "/fortnite";
 export const DASHBOARD = "/dashboard";
 export const NONE = "/none";
 export const LOADER = "/not-found";
