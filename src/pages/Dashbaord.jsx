bbefe86b9994ab1e45fd8bf0c3db32cb2dd6f0a0
dardan743 <dardan.llapashtica@riinvest.net>
2023-06-22 03:21:40 +0200

    dashbrod

--- src/pages/Dashbaord.jsx
+++ src/pages/Dashbaord.jsx
@@ -2,8 +2,58 @@ import React from "react";
 
 const Dashbaord = () => {
   return (
-    <div>
-      <div>Dashbaord</div>
+    <div className="flex items-center gap-10">
+      <div className="bg-white p-5 w-[30%]">
+        <h1 className="text-gray font-[800] text-3xl">Dropshipping</h1>
+        <h1 className="text-gray font-[800]">Learning</h1>
+        <ul className="ml-3">
+          <li>- Imam Gadzhi</li>
+          <li>- Sebastian Gjiorghiu</li>
+          <li>- Alex Hormozi</li>
+          <li>- Austin Rabin</li>
+        </ul>
+        <h1 className="text-gray font-[800]">Producs:</h1>
+        <ul className="ml-3">
+          <li>- Sopa Brush</li>
+          <li>- Charger Lamp</li>
+          <li>- Spinner Fidget</li>
+          <li>- Austin Rabin</li>
+          <li>- Spinner Airpods Case</li>
+          <li>- Custom Air Forces</li>
+        </ul>
+        <h1 className="text-gray font-[800]">Check Products Sold & Unsold:</h1>
+        <ul className="ml-3">
+          <li>- app.dropship.oi</li>
+          <li>- Pipiads</li>
+          <li>- Hellium 10</li>
+        </ul>
+      </div>
+
+      <div className="bg-white p-5 w-[30%] mt-5">
+        <h1 className="text-gray font-[800] text-3xl">Payments</h1>
+        <h1 className="text-gray font-[800]">
+          Subscriptions & Payments Monthly
+        </h1>
+        <ul className="ml-3">
+          <li>- Snapchat Premium: 0.50 CENT</li>
+          <li>- Spotify: 6.50 EUR - 16th of every month</li>
+          <li>- Vala: 14 EUR</li>
+          <li>- Credit Gjirafa: 137 EUR - Shum totale = 1,640 EUR</li>
+          <li>- Credit Neptun: 15 EUR - Shum totale = 338 EUR</li>
+          <li>- Tjerat: 300 EUR</li>
+        </ul>
+        <p>
+          Subscriptions & Payments Monthly - Snapchat Premium: 0.50 CENT -
+          Spotify: 6.50 EUR - 16th of every month - Vala: 14 EUR - Qethja: 21
+          EUR - Credit Gjirafa: 137 EUR - Shum totale = 1,640 EUR - Credit
+          Neptun: 15 EUR - Shum totale = 338 EUR - Tjerat: 300 EUR Pagesa e
+          kredise per muaj: 152 EUR Rroga: 600 EUR Pagesat totale per muaj: 494
+          EUR Kursimet: 100 EUR Borxh: - Gulii: 200 EUR - Granit: 100 EUR -
+          Erjon: 300 EUR Totali i borxhit: 600 EUR Kredia: Totali: 5,000 EUR
+          Totali me kamat: 6,000 Totali i pageses per muaj per 5 vjet: 125 EUR
+          Totali i pageses per muaj per 6 vjet: 100 EUR
+        </p>
+      </div>
     </div>
   );
 };
